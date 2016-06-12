e317a7b037d71c59281f7b47bb65dd1993e32a50
Darren Hall <djdarrenh@gmail.com>
2016-06-12 18:04:30 +0100

    update user input error message

--- classes/input.rb
+++ classes/input.rb
@@ -8,6 +8,14 @@ class Input
 	end
 
 
+	# print error message and get another number from user
+	def print_error_and_get_new_input_from_user
+		print "Hey! That is not a valid number. Try again:"
+		user_input = gets.chomp
+		validate_user_input( user_input )
+	end
+
+
 	# Check if user input is an integer with no decimal places
 	def validate_user_input(user_input)
 
@@ -21,9 +29,7 @@ class Input
 		if is_numeric? user_input
 			return { "result" => true, "user_input" => user_input }
 		else
-			print "Hey! That is not a valid number. Try again:"
-			user_input = gets.chomp
-			validate_user_input( user_input )
+			print_error_and_get_new_input_from_user
 		end
 
 	end
